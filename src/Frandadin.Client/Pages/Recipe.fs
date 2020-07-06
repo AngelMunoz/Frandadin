29f076d2ba9da9bcd578e8864d96abd54db3a212
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2020-07-05 20:01:15 -0600

    add preliminary recipe page

---
+++ src/Frandadin.Client/Pages/Recipe.fs
@@ -0,0 +1,54 @@
+﻿namespace Frandadin.Client.Pages
+
+
+module Recipe = 
+    open Elmish
+    open Bolero
+    open Bolero.Remoting
+    open Bolero.Remoting.Client
+    open Bolero.Templating.Client
+    open Frandadin.Client.Services
+    open Microsoft.AspNetCore.Components
+    open Frandadin.Client.Types
+
+    type State =
+        { id: Option<int>; recipe: Option<Recipe>; error: Option<string> }
+
+    type Msg =
+        | GetRecipe of Option<int>
+        | RecvRecipe of Option<Recipe>
+        | Error of exn
+
+    let private init recipeId = 
+        { id = recipeId; recipe = None; error = None }, Cmd.none
+
+    let private update (msg: Msg) (state: State) (recipes: RecipeService) : State * Cmd<Msg> = 
+        match msg with 
+        | GetRecipe id -> 
+            let cmd = 
+                match id with
+                | Some id ->
+                    Cmd.ofAuthorized recipes.findOne id RecvRecipe Error
+                | None -> Cmd.none
+            state, cmd
+        | RecvRecipe recipe ->
+            { state with recipe = recipe }, Cmd.none
+        | Error RemoteUnauthorizedException ->
+            { state with error = Some "You have been logged out." }, Cmd.none
+        | Error exn -> { state with error = Some exn.Message }, Cmd.none
+
+    let private view (state: State) (dispatch: Msg -> unit) = 
+        Html.article [ Classes [ "fran-page" ] ] [ Html.textf "Recipe %A!" state.id ]
+
+    type RecipePage() =
+        inherit ProgramComponent<State, Msg>()
+
+        [<Parameter>]
+        member val recipeId : Option<int> = None with get, set
+
+        override this.Program =
+            let recipeService = this.Remote<RecipeService>()
+            let update (msg: Msg) (state: State) = update msg state recipeService
+            let init _ = 
+                init this.recipeId
+            Program.mkProgram init update view
