5a89b0f6a96579e040593fffc4f1f5892e7285ab
Angel Daniel Munoz Gonzalez <scythes.lol@gmail.com>
2020-07-05 20:03:06 -0600

    add database module

---
+++ src/Frandadin.Server/Database.fs
@@ -0,0 +1,18 @@
+﻿namespace Frandadin.Server
+
+module Database =
+    open System
+    open Npgsql.FSharp
+
+    let private getEnv (name: string) =
+        Environment.GetEnvironmentVariable name |> Option.ofObj
+
+    let defaultConnection =
+        let connection = 
+            match getEnv "DATABASE_URL" with 
+            | Some url -> url
+            | None -> "postgresql://admin:Admin123@localhost:5432/frandadindb"
+        Sql.fromUri (Uri connection)
+
+
+    
